95ec672729b3dacf308177ac2d30a77eeddfafa2
Alessandro Lodi <alelod96@gmail.com>
2022-04-28 00:20:12 +0200

    Some state experiments

--- src/components/sudoku-grid/SudokuOuterGrid.tsx
+++ src/components/sudoku-grid/SudokuOuterGrid.tsx
@@ -1,11 +1,15 @@
 import classNames from 'classnames'
-import { FunctionComponent } from 'react'
+import { FunctionComponent, useState } from 'react'
 
 const SudokuCell: FunctionComponent<{
   isSelected?: boolean
-}> = ({ isSelected = false }) => {
+  onClick: () => void
+}> = ({ isSelected = false, onClick }) => {
   return (
-    <div className={classNames('h-20 w-20', isSelected ? 'bg-red-200' : '')}>
+    <div
+      onClick={onClick}
+      className={classNames('h-20 w-20', isSelected ? 'bg-red-200' : '')}
+    >
       <div className="flex h-full w-full justify-center items-center font-bold text-4xl">
         1
       </div>
@@ -13,47 +17,45 @@ const SudokuCell: FunctionComponent<{
   )
 }
 
-const SudokuInnerGrid: FunctionComponent = () => {
+const SudokuInnerGrid: FunctionComponent<{
+  cellSelected: number
+  onCellSelected: (cellSelected: number) => void
+}> = ({ cellSelected, onCellSelected }) => {
+  const arr = Array(3).fill(1)
   return (
     <div className="grid grid-cols-1 divide-y-2 divide-gray-200">
-      <div className="grid grid-cols-3 divide-x-2 divide-gray-200">
-        <SudokuCell />
-        <SudokuCell />
-        <SudokuCell />
-      </div>
-      <div className="grid grid-cols-3 divide-x-2 divide-gray-200">
-        <SudokuCell />
-        <SudokuCell />
-        <SudokuCell />
-      </div>
-      <div className="grid grid-cols-3 divide-x-2 divide-gray-200">
-        <SudokuCell />
-        <SudokuCell />
-        <SudokuCell />
-      </div>
+      {arr.map(() => (
+        <div className="grid grid-cols-3 divide-x-2 divide-gray-200">
+          {arr.map(() => (
+            <SudokuCell
+              onClick={() => {
+                onCellSelected(3)
+              }}
+            />
+          ))}
+        </div>
+      ))}
     </div>
   )
 }
 
 const SudokuOuterGrid: FunctionComponent = () => {
+  const [cellSelected, setCellSelected] = useState(0)
+  const arr = Array(3).fill(1)
   return (
     <div>
+      {cellSelected}
       <div className="grid grid-cols-1 divide-y-[6px] divide-gray-300 border-[6px] border-black">
-        <div className="grid grid-cols-3 divide-x-[6px] divide-gray-300">
-          <SudokuInnerGrid />
-          <SudokuInnerGrid />
-          <SudokuInnerGrid />
-        </div>
-        <div className="grid grid-cols-3 divide-x-[6px] divide-gray-300">
-          <SudokuInnerGrid />
-          <SudokuInnerGrid />
-          <SudokuInnerGrid />
-        </div>
-        <div className="grid grid-cols-3 divide-x-[6px] divide-gray-300">
-          <SudokuInnerGrid />
-          <SudokuInnerGrid />
-          <SudokuInnerGrid />
-        </div>
+        {arr.map(() => (
+          <div className="grid grid-cols-3 divide-x-[6px] divide-gray-300">
+            {arr.map(() => (
+              <SudokuInnerGrid
+                cellSelected={cellSelected}
+                onCellSelected={setCellSelected}
+              />
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   )
