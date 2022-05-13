ab071ddebbaa3522728a1878f864cdba969a2d41
Alessandro Lodi <alelod96@gmail.com>
2022-05-13 21:01:52 +0200

    PWA

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
 import React, { ReactElement } from 'react'
 import { Outlet } from 'react-router'
+import { useRegisterSW } from 'virtual:pwa-register/react'
+import ReloadPrompt from './components/pwa/ReloadPrompt'
 
 function App(): ReactElement {
-  return <Outlet />
+  const intervalMS = 60 * 60 * 1000
+
+  const updateServiceWorker = useRegisterSW({
+    onRegistered(r) {
+      r &&
+        setInterval(() => {
+          r.update()
+        }, intervalMS)
+    }
+  })
+
+  return (
+    <div>
+      <ReloadPrompt />
+      <Outlet />
+    </div>
+  )
 }
 
 export default App
